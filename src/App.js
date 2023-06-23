f29a69d79f373aa2dac981ca948f8aa6d72e756b
Amin Khodabande <whfk233@gmail.com>
2023-06-23 13:55:06 +0330

    basic chess board created

--- src/App.js
+++ src/App.js
@@ -1,7 +1,33 @@
-import './App.css'
-
+import { useState } from 'react'
+import { Chessboard } from 'react-chessboard'
+import Chess from 'chess.js'
 function App() {
-	return <div className="App"></div>
-}
+	const [game, setGame] = useState(new Chess())
 
+	// perform modify function on game state
+	function safeGameMutate(modify) {
+		setGame((g) => {
+			const update = { ...g }
+			modify(update)
+			return update
+		})
+	}
+
+	function onDrop(sourceSquare, targetSquare) {
+		let move = null
+		safeGameMutate((game) => {
+			move = game.move({
+				from: sourceSquare,
+				to: targetSquare,
+				promotion: 'q',
+			})
+		})
+
+		// illegal move made
+		if (move === null) return false
+    
+		return true
+	}
+	return <Chessboard position={game.fen()} onPieceDrop={onDrop} boardWidth='600' boardOrientation='black' />
+}
 export default App
